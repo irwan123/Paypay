bc775f09960c616f05fd9a501e847bd5935837ee
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-14 15:59:16 +0700

    mainactivity progress

--- app/src/main/java/com/cloudless/paypay/ui/main/MainActivity.kt
+++ app/src/main/java/com/cloudless/paypay/ui/main/MainActivity.kt
@@ -2,6 +2,11 @@ package com.cloudless.paypay.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.cloudless.paypay.R
 import com.cloudless.paypay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +19,12 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavigation.background = null
         binding.bottomNavigation.menu.getItem(2).isEnabled = false
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_favorit, R.id.navigation_notifications, R.id.navigation_account
+        ).build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/cloudless/paypay/ui/notification/NotificationsFragment.kt
@@ -0,0 +1,20 @@
+package com.cloudless.paypay.ui.notification
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cloudless.paypay.R
+
+
+class NotificationsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+}
