979d65f6af6f1f65dd1ba11299f7ceca0d3b81d2
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-14 18:52:06 +0700

    mainactivity progress

--- app/src/main/java/com/cloudless/paypay/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cloudless/paypay/ui/home/HomeFragment.kt
@@ -5,16 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.cloudless.paypay.R
+import com.cloudless.paypay.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+
+    private lateinit var fragmentHomeBinding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        fragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return fragmentHomeBinding.root
     }
 }
