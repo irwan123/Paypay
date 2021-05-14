76bef5a05b81177b9de964c35978fa7d5513e78a
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-14 14:07:50 +0700

    mainactivity progress

--- app/src/main/java/com/cloudless/paypay/ui/main/MainActivity.kt
+++ app/src/main/java/com/cloudless/paypay/ui/main/MainActivity.kt
@@ -3,10 +3,17 @@ package com.cloudless.paypay.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cloudless.paypay.R
+import com.cloudless.paypay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.menu.getItem(2).isEnabled = false
     }
 }
