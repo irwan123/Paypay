7ca2ceec3a70c50fd686a3138c1b7455ab2088f4
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-15 14:58:38 +0700

    mainactivity progress

--- app/src/main/java/com/cloudless/paypay/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cloudless/paypay/ui/home/HomeFragment.kt
@@ -1,16 +1,23 @@
 package com.cloudless.paypay.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cloudless.paypay.R
 import com.cloudless.paypay.databinding.FragmentHomeBinding
+import com.synnapps.carouselview.ImageListener
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var fragmentHomeBinding: FragmentHomeBinding
+    private val image = arrayOf<Int>(
+        R.drawable.promo_banner1,
+        R.drawable.promo_banner2,
+        R.drawable.promo_banner3
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +25,20 @@ class HomeFragment : Fragment() {
     ): View {
         fragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater, container, false)
         return fragmentHomeBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val carouselView = fragmentHomeBinding.carouselView
+        carouselView.pageCount = image.size
+
+        val imageListener =
+            ImageListener { position, imageView ->
+                imageView.setImageResource(
+                    image[position]
+                )
+            }
+        carouselView.setImageListener(imageListener)
     }
 }
