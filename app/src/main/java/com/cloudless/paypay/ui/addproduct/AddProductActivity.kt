3406000621efd5d61fe0c899ae96b6f870e79811
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-05-18 14:38:33 +0700

    add new

---
+++ app/src/main/java/com/cloudless/paypay/ui/addproduct/AddProductActivity.kt
@@ -0,0 +1,12 @@
+package com.cloudless.paypay.ui.addproduct
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cloudless.paypay.R
+
+class AddProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product)
+    }
+}
